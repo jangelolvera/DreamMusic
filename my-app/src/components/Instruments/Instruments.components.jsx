166e50d45d6c4340774af9c2401e690d66497c03
jangelolvera <a01275136@tec.mx>
2022-05-11 12:15:25 -0500

    Search bar

--- my-app/src/components/Instruments/Instruments.components.jsx
+++ my-app/src/components/Instruments/Instruments.components.jsx
@@ -13,10 +13,12 @@ function Instruments(props) {
     const onOpenModal = () => setOpen(true);
     const onCloseModal = () => setOpen(false);
     const [modalArt, setModalArt] = useState([]);
+    const [query, setQuery] = useState("");
+
     const fun = article => {
         onOpenModal();
         setModalArt(article);
-        console.log(article.descripcion)
+        console.log(article.id)
     }
 
     const details = (
@@ -48,58 +50,75 @@ function Instruments(props) {
     );
 
     return (
-        <>
-            {props.products.map((article) => {
+        <div id="general">
 
-                return (
+            <nav id="topBar">
+                <input id="inputQuery" placeholder="¿Buscas algún instrumento?" onChange={event => setQuery(event.target.value)} />
 
-                    <div className="App" key={article.instrumento} onClick={() => fun(article)}>
-                        <div id="instTable" key={article.instrumento}>
-                            <div id="artSquare"
-                                className="artTabs"
-                                type="button"
-                                key={article.instrumento}
-                                name={article.instrumento}
-                                style={{
-                                    width: '305px',
-                                    height: '490px',
-                                    margin: '2px solid black',
-                                }}
-                            >
-                                <img
-                                    className="imagesDisplay"
+            </nav>
+
+            <div id="grid">
+                {
+                props.products.filter(article => {
+                    if (query === "") {
+                        //if query is empty
+                        return article;
+                    } else if (article.instrumento.toLowerCase().includes(query.toLowerCase())) {
+                        //returns filtered array
+                        return article;
+                    }
+                }).map((article) => {
+
+                    return (
+
+                        <div className="App" key={article.instrumento} onClick={() => fun(article)}>
+                            <div id="instTable" key={article.instrumento}>
+                                <div id="artSquare"
+                                    className="artTabs"
+                                    type="button"
                                     key={article.instrumento}
                                     name={article.instrumento}
-                                    alt={article.instrumento}
-                                    src={article.url}
                                     style={{
-                                        width: '250px',
-                                        height: '280px',
-                                        marginBottom: '10px',
-
+                                        width: '305px',
+                                        height: '490px',
+                                        margin: '2px solid black',
                                     }}
-                                />
+                                >
+                                    <img
+                                        className="imagesDisplay"
+                                        key={article.instrumento}
+                                        name={article.instrumento}
+                                        alt={article.instrumento}
+                                        src={article.url}
+                                        style={{
+                                            width: '250px',
+                                            height: '280px',
+                                            marginBottom: '10px',
 
-                                <p id="artName">{article.instrumento}</p>
-                                <p id="artGen">{article.genero}</p>
-                                <p id="artP">{"$" + article.precio}</p>
+                                        }}
+                                    />
+
+                                    <p id="artName">{article.instrumento}</p>
+                                    <p id="artGen">{article.genero}</p>
+                                    <p id="artP">{"$" + article.precio}</p>
+                                </div>
                             </div>
+
                         </div>
 
-                    </div>
 
 
+                    );
 
-                );
 
-
-            })}
+                })}
+            </div>
 
             <Modal open={open} onClose={onCloseModal} center>
                 {details}
             </Modal>
 
-        </>
+        </div>
     );
 }
 
--- my-app/src/components/index.js
+++ my-app/src/components/index.js
@@ -1,1 +1,2 @@
 export {default as Instruments} from './Instruments/Instruments.components';
+
